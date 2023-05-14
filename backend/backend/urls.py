454bd06155d6296448a7ad68f28f5e4d09b82479
Zoclhas <zoclhas@gmail.com>
2023-05-14 14:32:02 +0400

    Add registration URL

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("dj-rest-auth/", include("dj_rest_auth.urls")),
+    path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
     path("api/", include("blog.urls")),
 ]
